0ec994305ad499ddf3caa2b6e2900eff83d0ac8f
Apisit Krutsri <k.apisit@outlook.com>
2022-11-21 13:27:36 +0700

    chore: config tailwind and add in index.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx,tsx,ts}"],
+  important: "#root",
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
