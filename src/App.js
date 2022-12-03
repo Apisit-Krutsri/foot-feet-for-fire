a2ec04606b36dbd1b61e520c84eb453a67556b43
PACHIKA38 <pachika.sr.61@ubu.ac.th>
2022-12-03 09:36:59 +0700

    chorn: update files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import AccountEdit from './pages/accountEdit/accountEdit'
+import React from "react";
+import ProfileEdit from "./pages/profileEdit/profileEdit";
+import ActivitySummary from "./pages/activitySummary/activitySummary";
 
 function App() {
-  return <div className='App'></div>;
+  return (<div className='App'>
+    <AccountEdit />
+    <ProfileEdit />
+  <ActivitySummary/>
+  </div>)
 }
 
 export default App;
---
+++ src/pages/activitySummary/Data.js
@@ -0,0 +1,13 @@
+export const UserData = [
+    {
+        id:1,year:2020,userGain:300,userLost:35,
+    },{
+        id:2,year:2022,userGain:800,userLost:65,
+    }
+    ,{
+        id:3,year:2019,userGain:200,userLost:25,
+    }
+    ,{
+        id:4,year:2018,userGain:500,userLost:90,
+    }
+]
